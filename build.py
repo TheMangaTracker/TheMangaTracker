f587271bf0edca9d1cb43fd41d100a723a861b4c
Yuri Kilochek <yuri.kilochek@gmail.com>
2015-07-13 22:12:04 +0300

    Some reorganization.

--- build.py
+++ build.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python
 # coding=UTF-8
 
-from PIL import Image
 from devtools import *
+
+for path in ORIGIN.glob('**/*.js'):
+    if path.is_file():
+        copy(TARGET / path.relative_to(ORIGIN), path)    
 
 for thing, url in [
     ('jquery.js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js'),
     ('angular.js', 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.2/angular.js'),
 ]: download(TARGET / 'thirdparty' / thing, url)
-
-for path in ORIGIN.glob('**/*.js'):
-    if path.is_file():
-        copy(TARGET / path.relative_to(ORIGIN), path)    
   
 copy(TARGET / 'icon.png', ORIGIN / 'icon.png')
 
-with (ORIGIN / 'version').open(encoding='UTF-8') as version_file:
-    version = version_file.read().strip()
-
-icons = [
-    (get_image_size(TARGET / 'icon.png'), 'icon.png')
-] 
-
 render(TARGET / 'manifest.json', ORIGIN / 'manifest.json.template',
-       version=version,
-       icons=icons)
+    version=(ORIGIN / 'version').open(encoding='UTF-8').read().strip(),
+    icons=[(get_image_size(TARGET / 'icon.png'), 'icon.png')],
+    scripts=['thirdparty/jquery.js'] + ['background.js'] + [s.relative_to(TARGET) for s in TARGET.glob('background/**/*.js')]     
+)
 
-scripts = [s.relative_to(TARGET) for s in TARGET.glob('**/*.js')]
-
-render(TARGET / 'index.html', ORIGIN / 'index.html.template',
-       scripts=scripts)
+render(TARGET / 'foreground.html', ORIGIN / 'foreground.html.template',
+    scripts=[
+        'thirdparty/jquery.js',
+        'thirdparty/angular.js',
+    ] + ['foreground.js'] + [s.relative_to(TARGET) for s in TARGET.glob('foreground/**/*.js')]
+)
 
 finish()
---
+++ source/foreground.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    TheMangaTracker = angular.module('TheMangaTracker', []);
+
+
+    
+    
+})();
