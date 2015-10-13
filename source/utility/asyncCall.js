028a05a8aa72c93fec1ee3c160afdac16fc8d664
Yuri Kilochek <yuri.kilochek@gmail.com>
2015-10-13 21:09:46 +0300

    Added id recycling to asyncCall.

--- source/utility/asyncCall.js
+++ source/utility/asyncCall.js
@@ -1,25 +1,41 @@
-let nextId = 0;
+let tag = 'asyncCall';
+
+let nextUnusedId = 0;
+let recycledIds = new Set();
 let callables = new Map();
 
-window.addEventListener('message', ({ source, data: { tag, id } }) => {
+window.addEventListener('message', ({ source, data }) => {
     if (source !== window) {
         return;    
     }    
    
-    if (tag != 'asyncCall') {
+    if (data.tag !== tag) {
         return; 
     }
     
-    let callable = callables.get(id);
-    callables.delete(id);
+    let callable = callables.get(data.id);
+
+    callables.delete(data.id);
+    recycledIds.add(data.id);
     
     callable();
 });
 
 export default function asyncCall(callable, ...args) {
-    let id = nextId++;
+    let id;
+    if (recycledIds.size === 0) {
+        id = nextUnusedId;
+        ++nextUnusedId;
+    } else {
+        for (let recycledId of recycledIds) {
+            id = recycledId;
+            recycledIds.delete(recycledId);
+            break;
+        }   
+    }
 
     callables.set(id, () => callable(...args));
 
-    window.postMessage({ tag: 'asyncCall', id }, '*');
+    window.postMessage({ tag, id }, '*');
 }
+
