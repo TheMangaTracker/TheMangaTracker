91620bd35edf89740122135d258cc6a47053dc3a
Yuri Kilochek <yuri.kilochek@gmail.com>
2015-11-04 01:53:41 +0300

    Improved naming of language-specific implementations of mangahere.co.

--- source/sites/mangahere.co/load.js
+++ source/sites/mangahere.co/load.js
@@ -1,22 +1,22 @@
 'use strict';
 
 define([
-    './difference.js', '/utility/AsyncStream.js', '/utility/http.js', 'jquery'
-], (   difference    ,           AsyncStream    ,           http    ,   $     ) => {
+    './languageSpecifics.js', '/utility/AsyncStream.js', '/utility/http.js', 'jquery'
+], (   languageSpecifics    ,           AsyncStream    ,           http    ,   $     ) => {
     function load(id) {
-        let language = id.slice(0, 2);
-        let name = id.slice(3);
-        let specific = difference[language];
-        let manga = http.get('http://' + specific.subdomain + '.mangahere.co/manga/' + name + '/')
+        let language = id.slice(0, id.indexOf('.'));
+        let name = id.slice(language.length + 1);
+        let languageSpecific = languageSpecifics[language];
+        let manga = http.get('http://' + languageSpecific.subdomain + '.mangahere.co/manga/' + name + '/')
             .map(document => {
                 let titles = (() => {
                     let main = $(document)
-                        .find('.manga_detail li label:contains("' + specific.summaryMarker + '")').text().trim()
-                        .slice(0, -specific.summaryMarker.length).trim()
+                        .find('.manga_detail li label:contains("' + languageSpecific.summaryMarker + '")').text().trim()
+                        .slice(0, -languageSpecific.summaryMarker.length).trim()
                     ;
                     let alternatives = $(document)
-                        .find('.manga_detail li:has(label:contains("' + specific.alternativeNameMarker + '"))').text().trim()
-                        .slice(specific.alternativeNameMarker.length).trim()
+                        .find('.manga_detail li:has(label:contains("' + languageSpecific.alternativeNameMarker + '"))').text().trim()
+                        .slice(languageSpecific.alternativeNameMarker.length).trim()
                     ;
                     alternatives = (alternatives === 'None') ? [] : alternatives.split('; ')
                     return [main, ...alternatives];
@@ -32,7 +32,7 @@ define([
                     return AsyncStream.from(urisAndTitles)
                         .map(([uri, title]) => {
                             let chapter = AsyncStream.of({
-                                id: specific.compressChapterId(/\/c([^\/]+)\/$/.exec(uri)[1]),
+                                id: languageSpecific.compressChapterId(/\/c([^\/]+)\/$/.exec(uri)[1]),
                                 uri,
 
                                 manga,
@@ -54,7 +54,7 @@ define([
                                         let firstPage = AsyncStream.of(extractPage(document));
                                         let otherPages = (() => {
                                             let options = $(document).find('.readpage_top .go_page .right option:not([selected])').toArray();
-                                            let uris = options.map(option => specific.resolvePageListUri($(option).prop('value')));
+                                            let uris = options.map(option => languageSpecific.resolvePageListUri($(option).prop('value')));
                                             return AsyncStream.from(uris)
                                                 .map(http.get).chain()
                                                 .map(extractPage)
--- source/sites/mangahere.co/search.js
+++ source/sites/mangahere.co/search.js
@@ -1,13 +1,13 @@
 'use strict';
 
 define([
-    './difference.js', '/utility/AsyncStream.js', '/utility/http.js', 'jquery'
-], (   difference    ,           AsyncStream    ,           http    ,   $     ) => {
+    './languageSpecifics.js', '/utility/AsyncStream.js', '/utility/http.js', 'jquery'
+], (   languageSpecifics    ,           AsyncStream    ,           http    ,   $     ) => {
     function search(query) {
         function searchInLanguage(language) {
-            let specific = difference[language];
+            let languageSpecific = languageSpecifics[language];
 
-            return http.get('http://' + specific.subdomain + '.mangahere.co/advsearch.htm')
+            return http.get('http://' + languageSpecific.subdomain + '.mangahere.co/advsearch.htm')
                 .map(document => {
                     let form = $(document).find('#searchform').clone();
                     
@@ -37,23 +37,23 @@ define([
                 }).chain()
                 .skipIf(document => $(document).find('.result_search .directory_footer').length === 0)  // nothing found
                 .map(function extractResults(document) {
-                    let resultsDiv = $(document).find('.result_search');
-                    let prefix = AsyncStream.from(resultsDiv.find('.manga_info').toArray().map(a => a.href))
+                    let results = $(document).find('.result_search');
+                    let fromFirstPage = AsyncStream.from(results.find('.manga_info').toArray().map(a => a.href))
                         .map(uri => {
                             let name = /\/([^\/]+)\/$/.exec(uri)[1];
                             return language + '.' + name;
                         })
                     ;
-                    let suffix = AsyncStream.from(resultsDiv.find('.next').toArray().map(a => a.href))
+                    let fromOtherPages = AsyncStream.from(results.find('.next').toArray().map(a => a.href))
                         .map(http.get).chain()
                         .map(extractResults).chain()
                     ;
-                    return prefix.chain(suffix);
+                    return fromFirstPage.chain(fromOtherPages);
                 }).chain()
             ;
         }
 
-        return AsyncStream.from(Object.keys(difference))
+        return AsyncStream.from(Object.keys(languageSpecifics))
             .keepIf(language => query.languages.has(language))
             .map(searchInLanguage).join()
         ;
